0304e2f274f6b3d31ae65865f7c60b1f5dcfd055
Ahmed Lekssays <ahmed@lekssays.com>
2021-03-12 16:09:19 +0100

    add sample malware

--- tests/generate_graph.py
+++ tests/generate_graph.py
@@ -16,8 +16,42 @@ def get_edges(neighbors: list):
             edges.append((peer, neighbor))
     return edges 
 
+def get_label(peer: str) -> str:
+    tmp = peer.split(".")
+    pid = tmp[0]
+    oid = tmp[1]
+
+    label = ""
+    if len(pid) == 5:
+        label += pid[-1]
+    elif len(pid) == 6:
+        label += pid[4:5]
+    
+    label += "/"
+
+    if len(oid) == 4:
+        label += oid[-1]
+    elif len(pid) == 5:
+        label += pid[3:4]
+
+    return label    
+
+def generate_labels(peers: list) -> dict:
+    labels = {}
+    for peer in peers:
+        labels[peer] = get_label(peer=peer)
+    return labels
+
 def generate_graph(neighbors: list):
     peers = list(neighbors.keys())
+    labels = generate_labels(peers=peers)
+    admins = []
+    normal = []
+    for peer in peers:
+        if "peer0" in peer:
+            admins.append(peer)
+        else:
+            normal.append(peer)
 
     G = nx.Graph()
     G.add_nodes_from(peers)
@@ -25,9 +59,16 @@ def generate_graph(neighbors: list):
     edges = get_edges(neighbors=neighbors)
 
     G.add_edges_from(edges)
+    pos = nx.planar_layout(G)
+    options = {"node_size": 50, "alpha": 0.8}
+    
+    nx.draw_networkx_nodes(G, pos, nodelist=admins, node_color="r", **options)
+    nx.draw_networkx_nodes(G, pos, nodelist=normal, node_color="y", **options)
+    nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.5)
+    nx.draw_networkx_labels(G,pos, labels, font_size=8, font_color='b')
 
-    nx.draw(G)
-    plt.savefig("network.png")
+    #plt.savefig("network.png")
+    plt.show()
 
 def main():
     neighbors = load_neighbors()
